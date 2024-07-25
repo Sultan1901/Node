8099f59b900996aedef47b233aa803d059d7a8ad
Sultan <s.alharbi@thegarage.sa>
2024-07-25 16:35:46 +0300

    add DZRT BOOT

--- src/test.js
+++ src/test.js
@@ -1,79 +1,26 @@
-// function findSumOrEven(num) {
-//   let odd = [];
-//   let even = [];
-//   let result = "";
-//   for (let i = 0; i < num; i++) {
-//     if (i % 2 == 0) {
-//       even.push(i);
-//     } else odd.push(i);
-//   }
+const request = require('request');
+const cheerio = require('cheerio');
 
-const { stringify } = require("querystring");
-
-//   let oddSum = odd.reduce((a, b) => a + b)
-//   let evenSum = even.reduce((a, b) => a + b);
-//   console.log(oddSum);
-//   console.log(evenSum);
-//   if (oddSum > evenSum) {
-//     result = "odd";
-//   } else if (oddSum < evenSum) {
-//     result = "even";
-//   } else result = "equal";
-//   return result;
-// }
-// console.log(60*60);
-// console.log(findSumOrEven(9));
-// function date_format(date) {
-//   let res = "";
-//   let arr = date.split("/");
-//   console.log(arr);
-
-//   res = date +" "+ "|" +" "+ arr[0] + "-" + arr[1] + "-" + arr[2]+" "+"|"+" "+ arr[1] + "/" + arr[2] + "/" + arr[0]
-//   return "'"+ res+"'"
-// }
-// console.log(date_format('2010/10/30'));
-// code war password = 3,Y%H}m2yp"@N5v
-// function past(h, m, s) {
-//   return s * 1000 + m * 60000 + h * 3600000;
-// }
-// console.log(past(1, 1, 1));
-// function sumArray(array) {
-//   return array
-//     ? array
-//         .sort((x, s) => x - s)
-//         .slice(1, -1)
-//         .reduce((a, b) => a + b, 0)
-//     : 0;
-// }
-// console.log(sumArray([1, 3]));
-// function incrementString(strng) {
-//   const body = strng.slice(0, -1);
-//   const lastDigit = strng.slice(-1).match(/[0-9]/);
-//   return lastDigit == null
-//     ? strng + "1"
-//     : lastDigit != 9
-//     ? body + (+lastDigit + 1)
-//     : incrementString(body) + "0";
-// }
-// console.log(incrementString("foobar999"));
-// const stringToNumber = function (str) {
-//   return Number(str);
-// };
-// console.log(stringToNumber("123"));
-// function shortcut(string) {
-//   return string.replace(/[ioaeu]/g, "");
-// }
-// console.log(shortcut("string"));
-
-function longest(s1, s2) {
-  s1arr = []
-  s = s1.split("").sort();
-  s.map((str)=>{
-
-      s1arr.push(str.indexOf(str))
-
-    
-  })
-  return s1arr ;
-}
-console.log(longest("sstring", "sssew"));
+var player = require('play-sound')((opts = {}));
+counter = 1;
+setInterval(() => {
+  const options = {
+    method: 'GET',
+    url: 'https://www.dzrt.com/ar/highland-berries.html',
+    headers: {
+      Cookie:
+        'PHPSESSID=am4md6ud9pd7cl9em11pm8nq1q; SERVERCORSID=fadede1407ead0f0f0bc7a14f61d778a|1721897641|1721897641; SERVERID=fadede1407ead0f0f0bc7a14f61d778a|1721897641|1721897641',
+    },
+  };
+  request(options, function (error, response) {
+    if (error) return new Error(error);
+    const $ = cheerio.load(response.body);
+    res = $('div')
+      .get()
+      .map((x) => $(x).attr('class'));
+    console.log(res[84] + ' ' + 'attempt' + ' ' + counter++);
+    res[84] != 'stock unavailable' && res[84] != undefined
+      ? player.play('../dzzrt.wav', () => {})
+      : null;
+  });
+}, 5000);
